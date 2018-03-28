1f39687799d068c1a035a218ad0ae4aaf0966e3f
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-03-28 23:02:47 +0200

    v1.0.1

--- src/app/01-rxjs-tasks/tasks/01-my-observables.ts
+++ src/app/01-rxjs-tasks/tasks/01-my-observables.ts
@@ -20,13 +20,23 @@ function example1() {
 
   // TODO 2: each subscribe call generating fn
 
-  // timeout$.subscribe(fullObserver('myTimeout'));
+  // timeout$.subscribe(fullObserver('example1'));
 }
 
+// TODO task: myFullObserver(tag)
+function myFullObserver(tag: string) {
+  return null;
+}
 
 // TODO task: myFromArray$
 export function myFromArray$(items: any[]): Observable<any> {
   return null; // TODO
+}
+
+function fromArrayTask() {
+  const names = ['bob', 'ed', 'kate'];
+  myFromArray$(names)
+    .subscribe(myFullObserver('fromArrayTask'));
 }
 
 // TODO task: myRange$
@@ -34,23 +44,54 @@ export function myRange$(startValue: number, count: number): Observable<number> 
   return null; // TODO
 }
 
+function rangeTask() {
+  myRange$(5, 7)
+    .subscribe(myFullObserver('rangeTask'));
+}
+
 // TODO task: myInterval$
 export function myInterval$(delayInMs: number): Observable<number> {
   return null; // TODO
 }
 
+function intervalTask() {
+  myInterval$(1000)
+    .subscribe(myFullObserver('intervalTask'));
+}
+
+function myFromArrayWithDelay$(items: any[], delayInMs: number) {
+  return null;
+}
+
+function fromArrayWithDelayTask() {
+  const values = [100, 200, 300];
+  myFromArrayWithDelay$(values, 800)
+    .subscribe(myFullObserver('fromArrayWithDelayTask'));
+}
+
+function myThrow$(error) {
+  return null;
+}
+
+function throwTask() {
+  myThrow$(new Error('Jakis blad!'))
+    .subscribe(myFullObserver('throwTask'));
+}
+
+
 // TODO task: myThrow$
 
-// TODO task: myFullObserver(tag)
-
-
 // more TODO
-// TODO task: myFromArrayWithDelay$
 
 // TODO task: myOf$
 
 // TODO task: myTimer$
 
 export function myObservablesApp() {
-  example1();
+  // example1();
+  // intervalTask();
+  // fromArrayTask();
+  // fromArrayWithDelayTask();
+  // throwTask();
+  // rangeTask();
 }
